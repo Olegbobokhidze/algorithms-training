18d323f6a4759bc751a66b4f3635c97ae4353b9b
Oleg Bobokhidze <oleg.bobokhidze.1@iliauni.edu.ge>
2023-02-17 16:53:17 +0400

    array to string with join method

--- script.js
+++ script.js
@@ -47,3 +47,10 @@ const findVowels2 = (str) => {
 };
 findVowels("blabla bleeee");
 findVowels2("blabla bleee");
+
+// Write a function that takes an array of words and smashes them together into a sentence and returns the sentence. You can ignore any need to sanitize words or add punctuation, but you should add spaces between each word. Be careful, there shouldn't be a space at the beginning or the end of the sentence!
+
+const getStringFromArr = (arr) => {
+  console.log(arr.join(" "));
+};
+getStringFromArr(["bla", "blue", "blee"]);
